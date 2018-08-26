d0fc28a436d165d73513133e1ef3de6c60119953
eiji03aero <eiji03aero@gmail.com>
2018-08-26 22:33:46 +0900

    added bins

---
+++ bin/initLib.sh
@@ -0,0 +1,13 @@
+# for add-apt-repository http://d.hatena.ne.jp/Kazuhira/20160116/1452933387
+sudo apt-get install apt-file
+sudo apt-file update
+sudo apt-file search add-apt-repository
+sudo apt-get install software-properties-common
+
+# Install the latest vim
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt install vim
+
+# Install tmux
+sudo apt install tmux
