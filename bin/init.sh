f31ec511e8aeef3b3b9d2888a138f9dd1903a0f6
eiji03aero <eiji03aero@gmail.com>
2019-06-10 00:16:30 +0900

    update

--- bin/init.sh
+++ bin/init.sh
@@ -9,6 +9,7 @@ ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
+ln -sf ~/dotfiles/.agignore ~/.agignore
 
 ln -sf ~/dotfiles/.vim/ ~/
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
